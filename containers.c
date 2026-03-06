6ce55b88e579d3de93d1d644b05289b3693b052c
yahya <yelmaoui@gmail.com>
2026-03-06 17:54:44 +0000

    Modify on containers.h and containers.c

--- containers.c
+++ containers.c
@@ -1,15 +1,43 @@
 #include "containers.h"
 
-GtkWidget* create_grid(Grid *gridConfig){
+GtkWidget* create_grid(Grid *config) {
     GtkWidget *grid = gtk_grid_new();
-    gtk_grid_set_column_spacing(GTK_GRID(grid),gridConfig->cl_spacing);
-    gtk_grid_set_row_spacing(GTK_GRID(grid),gridConfig->r_spacing);
-    gtk_widget_set_margin_top(grid,gridConfig->margin_top);
+
+    gtk_grid_set_column_spacing(GTK_GRID(grid), config->column_spacing);
+    gtk_grid_set_row_spacing(GTK_GRID(grid), config->row_spacing);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), config->column_homogeneous);
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), config->row_homogeneous);
+
+    gtk_widget_set_halign(grid, config->halign);
+    gtk_widget_set_valign(grid, config->valign);
+    gtk_widget_set_hexpand(grid, config->hexpand);
+    gtk_widget_set_vexpand(grid, config->vexpand);
+
+    gtk_widget_set_margin_top(grid, config->margin_top);
+    gtk_widget_set_margin_bottom(grid, config->margin_bottom);
+    gtk_widget_set_margin_end(grid, config->margin_end);
+    gtk_widget_set_margin_start(grid, config->margin_start);
+
     return grid;
 }
 
-GtkWidget* create_box(Box *boxConfig){
-    GtkWidget *box = gtk_box_new(boxConfig->orientation,boxConfig->spacing);
-    gtk_box_set_homogeneous(GTK_BOX(box),boxConfig->homogeneous);
+void add_to_grid(GtkWidget *grid, GtkWidget *child, GridChild *pos) {
+    int w = (pos->width > 0) ? pos->width : 1;
+    int h = (pos->height > 0) ? pos->height : 1;
+    gtk_grid_attach(GTK_GRID(grid), child, pos->column, pos->row, w, h);
+}
+
+GtkWidget* create_box(Box *boxConfig) {
+    GtkWidget *box = gtk_box_new(boxConfig->orientation, boxConfig->spacing);
+    gtk_box_set_homogeneous(GTK_BOX(box), boxConfig->homogeneous);
+    gtk_widget_set_margin_top(box, boxConfig->margin_top);
+    gtk_widget_set_margin_bottom(box, boxConfig->margin_bottom);
+    gtk_widget_set_margin_start(box, boxConfig->margin_start);
+    gtk_widget_set_margin_end(box, boxConfig->margin_end);
+    gtk_widget_set_halign(box, boxConfig->halign);
+    gtk_widget_set_valign(box, boxConfig->valign);
+    gtk_widget_set_hexpand(box, boxConfig->hexpand);
+    gtk_widget_set_vexpand(box, boxConfig->vexpand);
+    
     return box;
 }
--- containers.h
+++ containers.h
@@ -4,17 +4,36 @@
 #include <gtk/gtk.h>
 
 typedef struct {
-    int cl_spacing;
-    int r_spacing;
-    int margin_top;
-}Grid;
+    int column_spacing;
+    int row_spacing;
+    bool column_homogeneous;
+    bool row_homogeneous;
+    GtkAlign halign;
+    GtkAlign valign;
+    bool hexpand;
+    bool vexpand;
+    int margin_top, margin_bottom, margin_start, margin_end;
+} Grid;
+
+typedef struct {
+    int column;  // The column number to attach the widget to (starting at 0)
+    int row;     // The row number to attach the widget to (starting at 0)
+    int width;   // How many columns the widget spans (default 1)
+    int height;  // How many rows the widget spans (default 1)
+} GridChild;
 
 typedef struct {
     GtkOrientation orientation;
     int spacing;
     bool homogeneous;
-}Box;
+    bool hexpand;
+    bool vexpand;
+    GtkAlign halign;
+    GtkAlign valign;
+    int margin_top, margin_bottom, margin_start, margin_end;
+} Box;
 
 GtkWidget* create_grid(Grid *gridConfig);
 GtkWidget* create_box(Box *boxConfig);
+void add_to_grid(GtkWidget *grid, GtkWidget *child, GridChild *pos);
 #endif
